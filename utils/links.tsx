eea7b0cd330fd3e0378e73c4cea84ea8c67cdc70
Srnishappy <suranatsuwannarat@gmail.com>
2025-06-09 22:33:40 +0700

    update Navlink

--- utils/links.tsx
+++ utils/links.tsx
@@ -7,4 +7,5 @@ export const links: NavLink[] = [
   { href: 'profile', label: 'Profile' },
   { href: 'favorite', label: 'Favorite' },
   { href: 'camp', label: 'Camp' },
+  { href: 'camp/create', label: 'Create' },
 ];
